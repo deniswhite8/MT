1473ddf9002cb4bb316386c72346ad3600fe6050
denis <deniswhite8@gmail.com>
2013-03-18 17:41:23 +0400

    fix

--- MT.js
+++ MT.js
@@ -1,72 +1,110 @@
 var head = 1;
 var map = {};
 var state = 0;
+var running = false;
 
 $(document).ready(function(){
 
 	var row = $("#row");
 	row.children().eq(head).addClass("selected");
 	
-	$("#ok").click(function() {
-		head = 1;
+	$("#ok").click(init);
+	init();
+	function init()
+	{
+		head = 2;
 		map = {};
 		state = 0;
+		running = false;
 	
 		var text = $("#string").val();
 		
 		row.empty();
+		row.append("<td>...</td>");
 		row.append("<td>&lambda;</td>");
 		for(var i = 0; i < text.length; i++)
 			row.append("<td>"+ text[i] +"</td>");
 		row.append("<td>&lambda;</td>");
+		row.append("<td>...</td>");
 		
 		row.children().eq(head).addClass("selected");
-	});
+	}
 	
 	$("#run").click(function() {
-		head = 1;
-		map = {};
-		state = 0;
-		var commands = $("#commands").val().replace(/ /g,'').split("\n");
-		for(var i = 0; i < commands.length; i++)
+		if(running)
 		{
-			var mas = commands[i].split("->");
-			
-			var move1 = mas[1][mas[1].length-1];
-			mas[1] = mas[1].substr(0, mas[1].length-1);
-			
-			var ch1 = mas[1][mas[1].length-1];
-			mas[1] = mas[1].substr(0, mas[1].length-1);
-			
-			mas[1] = mas[1].substr(1);
-			var n1 = mas[1];
-			
-			map[mas[0]] = {n:n1, ch:ch1, move:move1};
+			running = false;
+			$("#ok").removeAttr("disabled");
+			$("#string").removeAttr("disabled");
+			$("#commands").removeAttr("disabled");
+			$("#run").attr('value', 'Старт');
+			init();
 		}
-		
-		while(state != "z")
+		else
 		{
-			var ch = row.children().eq(head).text();
-			var next = map["q"+state+ch];
-			if(next === undefined) alert("Not next instruction!");
-			state = next.n;
-			row.children().eq(head).text(next.ch);
-			row.children().eq(head).removeClass("selected");
-			if(next.move == "R") 
+			$("#ok").attr("disabled", "disabled");
+			$("#string").attr("disabled", "disabled");
+			$("#commands").attr("disabled", "disabled");
+			$("#run").attr('value', 'Стоп');
+		
+			running = true;
+			var commands = $("#commands").val().replace(/ /g,'').split("\n");
+			for(var i = 0; i < commands.length; i++)
 			{
-				head++;
-				if(head == row.children().length-1) row.children().eq(head).after("<td>&lambda;</td>");
+				if(commands[i] == "") continue;
+				
+				var mas = commands[i].split("->");
+			
+				var move1 = mas[1][mas[1].length-1];
+				mas[1] = mas[1].substr(0, mas[1].length-1);
+			
+				var ch1 = mas[1][mas[1].length-1];
+				mas[1] = mas[1].substr(0, mas[1].length-1);
+			
+				mas[1] = mas[1].substr(1);
+				var n1 = mas[1];
+			
+				map[mas[0]] = {n:n1, ch:ch1, move:move1};
 			}
-			else if(next.move == "L")
-			{
-				head--;
-				if(head == 0) row.children().eq(head).before("<td>&lambda;</td>");
-				head = 1;
-			}
-			else if(next.move != "E") alert("R, L or E!");
-			row.children().eq(head).addClass("selected");
+		
+			setTimeout(run, 1000);
 		}
 		
 	});
+	
+	function run()
+	{
+		if(state != "z" && running)
+		{
+			step();
+			setTimeout(run, 1000);
+		}
+	}
+
+	function step()
+	{
+		var ch = row.children().eq(head).text();
+		var next = map["q"+state+ch];
+		if(next === undefined) alert("Not next instruction!");
+		state = next.n;
+		row.children().eq(head).text(next.ch);
+		row.children().eq(head).removeClass("selected");
+		if(next.move == "R") 
+		{
+			head++;
+			if(head == row.children().length-2) row.children().eq(head).after("<td>&lambda;</td>");
+		}
+		else if(next.move == "L")
+		{
+			head--;
+			if(head == 1)
+			{
+				row.children().eq(head).before("<td>&lambda;</td>");
+				head = 2;
+			}
+		}
+		else if(next.move != "E") alert("R, L or E!");
+		row.children().eq(head).addClass("selected");
+	}
 	
 });
